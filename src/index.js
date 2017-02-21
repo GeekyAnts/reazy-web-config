4c972c6ceea362c5fd5cc9b6d7c5b4f6c438deae
Himanshu <himanshu@sahusoft.com>
2017-02-21 19:19:53 +0530

    remove log

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ export default function(params) {
   return function(serviceName) {
     const app = this;
 
-    // const env = require(params.envPath);
-    console.log('env', params.env);
-
     app[serviceName] = params.env;
 
     return params.env;
